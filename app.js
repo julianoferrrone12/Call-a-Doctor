82127bc7095fa6d9ab03f1355e0b7493386b26e6
Fhilipfhc <fhilipfhc@gmail.com>
2021-12-05 06:52:02 -0300

    Quarto commit desafio Castlethon

--- app.js
+++ app.js
@@ -18,32 +18,3 @@ sign_up_btn.addEventListener('click', () => {
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 })
-
-function customValidation(event) {
-    const field = event.target
-    
-    function verifyErrors() {
-        let foundError = false;
-    
-        for(let error in field.validity) {
-            // se não for customError
-            // então verifica se tem erro
-            if (field.validity[error] && !field.validity.valid ) {
-                foundError = error
-            }
-        }
-        return foundError;
-    }
-    const error = verifyErrors() 
-
-    if (error) {
-        field.setCustomValidity("Esse campo é obrigatoiro")
-    }else {
-        field.setCustomValidity("")
-    }
-    
-}
-
-for(field of fields) {
-    field.addEventListener("invalid", customValidation)
-}
